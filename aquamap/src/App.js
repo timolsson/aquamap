54b899755012c33809dbe7d83bb1bb48b31cf28f
Tim Olsson <tim.olsson@hyperisland.se>
2020-07-14 20:06:00 +0200

    Setup and header
    Installed react and created a Header component

---
+++ aquamap/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header.js";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ aquamap/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="App-header">
+      <h1>aquaMap - Your local water guide.</h1>
+    </header>
+  );
+}
+
+export default Header;
